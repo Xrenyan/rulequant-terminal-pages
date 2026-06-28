c846e1aff3821a985310ab56655702b28bc691ec
RuleQuant <rulequant-local@example.com>
2026-06-28 15:19:33 +0800

    Deploy latest RuleQuant static site

---
+++ _next/static/chunks/app/dashboard/page-ee724c10cd32117c.js
@@ -0,0 +1,9 @@
+// runtime can't be in strict mode because a global variable is assign and maybe created.
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([[105],{},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, [430,543,383,600,274,358], () => (__webpack_exec__(3398)));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
