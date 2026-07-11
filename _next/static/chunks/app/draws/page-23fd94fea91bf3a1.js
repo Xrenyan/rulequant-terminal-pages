6f9e88e5442fea03ba97a707b1fd5a27a59bb65c
RuleQuant <rulequant-local@example.com>
2026-07-11 21:46:30 +0800

    Deploy latest RuleQuant static site

---
+++ _next/static/chunks/app/draws/page-23fd94fea91bf3a1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[798],{},_=>{_.O(0,[852,374,398,600,274,358],()=>_(_.s=3398)),_N_E=_.O()}]);
--- _next/static/chunks/app/sample-check/page-3077cd7dfd078734.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[236],{},_=>{_.O(0,[433,890,57,147,204,358],()=>_(_.s=4057)),_N_E=_.O()}]);
